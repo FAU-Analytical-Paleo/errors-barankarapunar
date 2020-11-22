bf82dd7c39304f9b850de7065c734991055f19b6
barankarapunar <barankarapunar@gmail.com>
2020-11-22 21:50:41 +0100

    ans2 and ans3 partly

--- Exercise3_BK.R
+++ Exercise3_BK.R
@@ -20,3 +20,26 @@ err_build <- round((25/2550)*(tan(rad_err)/tan(rad_ans)), 4)
 
 print(paste0("The height of the building is ", h_build, " +- ", err_build, " meters"))}
 
+# Answer 2
+
+print(paste0("The longevity of the volcanic activity is ", 29.66-25.53, " +- ", 0.1+0.2, " My"))
+
+# Answer 3
+
+eqs <- read.delim("C:/Users/Baran/Desktop/Computer in Geosciences Course/Week3_Errors/Exercise/ex3_eqscals.txt", sep = "", header = F)
+
+colnames(eqs) <- c("x", "r", "Mo") # "X(km)", "r(m)", "Mo(Nm)"
+
+#3a)
+
+#mean, median, standard deviation, and median absolute deviation (MAD) for r and Mo? 
+
+mean(eqs$r)
+median(eqs$r)
+sd(eqs$r)
+mad(eqs$r)
+
+mean(eqs$Mo)
+median(eqs$Mo)
+sd(eqs$Mo)
+mad(eqs$Mo)
